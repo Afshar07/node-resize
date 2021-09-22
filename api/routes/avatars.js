338e7a7a2dcde79815f4561cc90e21a0b13f838b
Afshar07 <mr.afshar0@gmail.com>
2021-09-22 14:23:04 +0330

    Create a node server and receive images through a small api and save them to uploads folder

---
+++ api/routes/avatars.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "uploads/");
+  },
+  filename: function (req, file, callback) {
+    callback(null, new Date().toISOString() + file.originalname);
+  },
+});
+const fileFilter = (req, file, callback) => {
+  if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+    callback(null, true);
+  } else {
+    callback(new Error("فرمت فایل ارسالی مناسب نیست"), false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 3,
+  },
+  fileFilter: fileFilter,
+});
+
+router.post("/", upload.single("avatar"), (req, res, next) => {
+  res.status(200).json({
+    message: "File Received",
+  });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const productRoutes = require("./api/routes/products");
+const avatarRoutes = require("./api/routes/avatars");
+
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  res.header("Acess-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
+app.use("/avatars", avatarRoutes);
+app.use("/products", productRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
