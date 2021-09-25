0b05c1f3c533ad3361255b3b4d7d68ee58e3c9c9
Afshar07 <mr.afshar0@gmail.com>
2021-09-25 12:42:49 +0330

    Fix a typo in header

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const avatarRoutes = require("./api/routes/avatars");
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-  res.header("Acess-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
