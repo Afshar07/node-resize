4fd5684e31545259f9e1b8bae5ba540dc193dcf7
Afshar07 <mr.afshar0@gmail.com>
2021-09-25 12:45:14 +0330

    Get the file from request, send it through resize module and then upload it to laravel

--- api/routes/avatars.js
+++ api/routes/avatars.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const multer = require("multer");
 const resize = require("../../resize");
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, "uploads/");
@@ -11,7 +14,11 @@ const storage = multer.diskStorage({
   },
 });
 const fileFilter = (req, file, callback) => {
-  if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg"
+  ) {
     callback(null, true);
   } else {
     callback(new Error("فرمت فایل ارسالی مناسب نیست"), false);
@@ -25,11 +32,42 @@ const upload = multer({
   fileFilter: fileFilter,
 });
 
-router.post("/", upload.single("avatar"), (req, res, next) => {
-  resize(req.file.path, "jpeg");
-  res.status(200).json({
-    message: "File Received",
-  });
+router.post("/", upload.single("avatar"), async (req, res, next) => {
+  let imgFormat = req.file.mimetype.split("/");
+  let newAvatarPath = await resize(req.file.path, req.file.mimetype);
+  const formData = new FormData();
+  let formHeaders = formData.getHeaders();
+  formData.append("avatar", fs.createReadStream(newAvatarPath));
+  try {
+    const response = await axios.post(
+      `${process.env.API_BASE}/api/user/avatar/upload`,
+
+      formData,
+
+      {
+        headers: {
+          Authorization: `Bearer ${req.body.token}`,
+          ...formHeaders,
+        },
+      }
+    );
+    if (response.status === 200) {
+      res.json(response.data);
+      fs.unlink(`./resized/resizedAvatar.${imgFormat[1]}`, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error.response);
+    res.status(error.response.status).json(error.response.data);
+    fs.unlink(`./resized/resizedAvatar.${imgFormat[1]}`, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
 });
 
 module.exports = router;
