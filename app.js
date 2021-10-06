af2d87613a0013921f048f1055b84075b62e6791
Afshar07 <mr.afshar0@gmail.com>
2021-10-06 10:20:55 +0330

    Change cors options

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const avatarRoutes = require("./api/routes/avatars");
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+    return res.status(200).json({});
+  }
   next();
 });
 
