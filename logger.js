1a3bd6c2aa32dc3fa915f61e56ad3fe704a960e1
Afshar07 <mr.afshar0@gmail.com>
2021-10-14 10:49:30 +0330

    Change filename and add TODO

--- logger.js
+++ logger.js
@@ -1,9 +1,11 @@
 const winston = require("winston");
 require("winston-daily-rotate-file");
 const transport = new winston.transports.DailyRotateFile({
-  filename: "logs/%DATE%/combine.log",
+  filename: "logs/%DATE%/avatars.log",
   datePattern: "YYYY-MM-DD",
 });
+
+// TODO: Send user id to log it
 
 const logger = winston.createLogger({
   level: "info",
