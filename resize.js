b3ccac3fefbd3e08382fb4a5bbf825a1aad1e59a
AmirAzizi Lenovo <amirazizimail@gmail.com>
2022-01-20 17:55:32 +0330

    hash

--- resize.js
+++ resize.js
@@ -5,7 +5,7 @@ const crypto = require("crypto");
 module.exports = async function resize(path, mimetype) {
   let imgFormat = mimetype.split("/");
   const md5hash = crypto.createHash("md5");
-  const filePath = `./resized/${hd5hash
+  const filePath = `./resized/${md5hash
     .update("resizedAvatar")
     .digest("hex")}.${imgFormat[1]}`;
 
